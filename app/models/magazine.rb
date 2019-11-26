81b1cae829f0d10a3ff2075b7ffeded8ae86f551
Miss-Cheese <nat.galligan@gmail.com>
2019-11-26 10:51:40 -0500

    Done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,55 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
+
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+
+  def self.find_by_name(name)
+    @@all.find {|magazine|
+    magazine.name == name
+    }
+  end
+
+  def magazine_articles
+    Article.all.select {|article|
+    article.magazine == self
+    }
+  end
+
+  def contributors
+    magazine_articles.map {|article|
+    article.author
+    }.uniq
+  end
+
+  def article_titles
+    magazine_articles.map {|article|
+    article.title
+    }
+  end
+
+
+  # def contributing_authors
+
+  # Returns an array of authors who have written 
+  # more than 2 articles for the magazine
+
+  #   author_array = magazine_articles.map {|article|
+  #   article.author}
+  #   author_array.max_by {|author|
+  #   author_array.count(author)}
+  # end
 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,11 +7,20 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author1 = Author.new("Arthur Conan Doyle")
+author2 = Author.new("Stephen King")
+author3 = Author.new("Elizabeth Gilbert")
 
+magazine1 = Magazine.new("National Geographic", "Science")
+magazine2 = Magazine.new("New Yorker", "News")
+magazine3 = Magazine.new("US Weekly", "Lifestyle")
 
-
-
-
+article1 = Article.new(author2, magazine3, "Shining")
+article2 = Article.new(author3, magazine2, "Eat, Pray, Love")
+article3 = Article.new(author1, magazine1, "Sherlock Holmes")
+article4 = Article.new(author1, magazine2, "Doctor Watson")
+article5 = Article.new(author3, magazine2, "Article #2")
+article6 = Article.new(author3, magazine2, "Article #3")
 
 
 
