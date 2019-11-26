6d7ce9b5566c302b1ed8eea76935fbd3809ed0dc
Alex Williams <alex_williams@protonmail.com>
2019-11-26 10:11:46 -0500

    Done.

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,33 @@
 class Magazine
   attr_accessor :name, :category
-
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
-
-
+  def contributors
+    articles = Article.all.select{|article| article.magazine == self}
+    (articles.map{|article| article.author}).uniq
+  end
+  def self.find_by_name(name)
+    @@all.find{|magazine| magazine.name == name}
+  end
+  def article_titles
+    (Article.all.select{|article| article.magazine == self}).map{|article| article.title}
+  end
+  def contributing_authors #####
+    articles = Article.all.select{|article| article.magazine == self}
+    articles = articles.map{|article| article.author}
+    authors = []
+    for x in articles do
+      if articles.count(x) > 1
+        authors << x
+      end
+    end
+    authors.uniq
+  end
+  def self.all
+    @@all
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,18 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
+auth1 = Author.new("Alex")
+auth2 = Author.new("Zalmy")
+auth3 = Author.new("Shakil")
+mag1 = Magazine.new("Publishers Weekly", "Literature")
+mag2 = Magazine.new("NY Review of Books", "Literature")
+mag3 = Magazine.new("Journal of Singularities", "Pure Math")
+art1 = Article.new(auth1, mag1, "Literature")
+art2 = Article.new(auth2, mag2, "Books")
+art3 = Article.new(auth3, mag3, "Abstract Algebra")
+art4 = Article.new(auth1, mag2, "More Literature")
+art5 = Article.new(auth2, mag3, "Topology")
+auth1.add_article(mag1, "Movies!")
+auth1.add_article(mag1, "Books!")
 
-###  WRITE YOUR TEST CODE HERE ###
-
-
-
-
-
-
-
-
-
-### DO NOT REMOVE THIS
 binding.pry
 
 0
