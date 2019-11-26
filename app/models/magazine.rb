63dd21fdb4dc8185ef08cce86b220f9453c3e7b5
MACorman <madelinecorman@gmail.com>
2019-11-26 10:51:14 -0500

    completed Mod1 code challenge

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,39 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select {|art| art.magazine == self}
+  end
+
+  def contributors
+    articles.map {|art| art.author}
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|mag| mag.name == name}
+  end
+
+  def article_titles
+    articles.map {|art| art.title}
+  end 
+
+  def contributing_authors
+    # still working - need to return key of hash (auth instance)
+     auth_count = Hash.new(0)
+    contributors.each_with_object(auth_count) {|auth, count| count[auth] +=1}
+    auth_count.select {|auth, count| count > 2}
+  end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -5,14 +5,23 @@ def reload
 end
 
 
-###  WRITE YOUR TEST CODE HERE ###
+auth1 = Author.new("Jerry")
+auth2 = Author.new("Elaine")
+auth3 = Author.new("George")
+auth4 = Author.new("Kramer")
 
+mag1 = Magazine.new("Vogue", "Fashion")
+mag2 = Magazine.new("Sports Illustrated", "Sports")
+mag3 = Magazine.new("Time", "General")
+mag4 = Magazine.new("Fishing Weekly", "Fishing")
 
-
-
-
-
-
+art1 = Article.new(auth1, mag3, "What's the Deal..?")
+art2 = Article.new(auth2, mag1, "Puffy Shirt")
+art3 = Article.new(auth3, mag2, "How I Landed My Job")
+art4 = Article.new(auth4, mag4, "Using Explosives to Fish")
+art5 = Article.new(auth1, mag3, "Comedy in NYC")
+art6 = Article.new(auth1, mag4, "Don't Fish With Newman")
+art7 = Article.new(auth1, mag3, "My Career: A Retrospective")
 
 
 ### DO NOT REMOVE THIS
