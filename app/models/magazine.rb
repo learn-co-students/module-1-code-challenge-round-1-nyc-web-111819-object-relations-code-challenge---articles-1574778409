59a130fb063518864f1479db58f2e08154f82667
kevinkim1030 <kevin.kim1030@gmail.com>
2019-11-26 10:51:08 -0500

    attempted code challenge

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,61 @@
 class Magazine
+
   attr_accessor :name, :category
 
 
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
 
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.magazine == self
+    end
+  end
+
+  def contributors
+    articles.map do |article|
+      article.author
+    end
+  end
+
+  def self.find_by_name(mag_name)
+    Magazine.all.find do |magazine|
+      magazine.name == mag_name
+    end
+  end
+
+  def article_titles
+    articles.map do |article|
+      article.title
+    end
+  end
+
+  def contributing_authors_name
+    contributors.map do |contributor|
+      contributor.name
+    end.sort
+  end
+
+  def contributing_authors
+    authors = []
+    contributing_authors_name.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total}
+    
+  end
 
 end
+
+# counts = Hash.new(0)
+# names.each { |name| counts[name] += 1 }
+
+# Magazine#contributing_authors
+# Returns an array of authors who have written 
+# more than 2 articles for the magazine
--- tools/console.rb
+++ tools/console.rb
@@ -6,12 +6,21 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+a1 = Author.new("alanna")
+a2 = Author.new("brody")
+a3 = Author.new("carl")
+a4 = Author.new("dana")
+
+m1 = Magazine.new("apple", "tech")
+m2 = Magazine.new("banana", "fruit")
+m3 = Magazine.new("cable", "internet")
+m4 = Magazine.new("ebay", "shopping")
 
 
-
-
-
-
+at1 = Article.new(a1, m2, "one day")
+at2 = Article.new(a2, m2, "one morning")
+at3 = Article.new(a4, m2, "one night")
+at4 = Article.new(a1, m2, "one evening")
 
 
 
